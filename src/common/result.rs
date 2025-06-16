4dc9182b0b6812be2d4f43135d111b6c3007a0d1
Heric da Silva Cruz <hericsilvaho@gmail.com>
2025-06-16 13:37:31 -0300

    adicionando graph_id na estrutura de resultado

--- src/common/result.rs
+++ src/common/result.rs
@@ -4,6 +4,7 @@ use uuid::Uuid;
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct TaskResult {
     pub task_id: Uuid,
+    pub graph_id: String,
     pub worker_id: Uuid,
     pub fitness: f64,
     pub solution_data: Vec<u8>,
